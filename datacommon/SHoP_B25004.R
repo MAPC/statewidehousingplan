91265a55914eb31acc0788f97784b07c58eced44
lberman <lberman@mapc.org>
2024-06-20 11:29:28 -0400

    update for moe_prop

--- datacommon/SHoP_B25004.R
+++ datacommon/SHoP_B25004.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidycensus)
 library(sf)
 library(geojsonsf)
 library(ggplot2)
@@ -29,8 +30,14 @@ get_b25004 <- read.csv(paste0("https://datacommon.mapc.org/csv?table=tabular.b25
 get_b25024 <- read.csv(paste0("https://datacommon.mapc.org/csv?table=tabular.b25024_hu_units_in_structure_acs_m&database=ds&years=",input_year,"&year_col=acs_year"))
 
 
+# 1.1 select columns
+#select_data <- get_data %>% 
+#  select(c(muni_id,municipal,geoid,logrecno,acs_year,vac_tot,vac_tot_m,))
+
+
+
 ## 2. merge tabular data on muni_id
-vac <- get_b25004 %>%
+calc <- get_b25004 %>%
   left_join(.,
             get_b25024 %>% select(c(muni_id,hu,hu_me)),
             by = c('muni_id' = 'muni_id')) %>% 
@@ -38,18 +45,20 @@ vac <- get_b25004 %>%
 
 
 ## 3.  run calculations (as needed)
+calc <- calc %>%  #percent of vacant units
+  mutate(vac_pct = (vac_tot/hu)*100) %>% 
+  mutate(vac_pct_m = round(moe_prop(vac_tot, hu, vac_tot_m, hu_me)*100, 2))
 
-vac <- vac %>%
-  mutate(vac_pct = (vac_tot/hu)*100) 
+calc <- calc %>%  #percent of units for sale or for rent
+  mutate(avail_pct = ((av_rent+av_sale)/hu)*100) %>% 
+  mutate(avail_pct_m = round(moe_prop((av_rent+av_sale), hu, ((av_rent_m + av_sale_m)), hu_me)*100, 2))
+
+# ? how to get moe_prop for sum of columns in previous line?
 
 
-# 1.1 select columns
-#select_data <- get_data %>% 
-#  select(c(muni_id,municipal,geoid,logrecno,acs_year,vac_tot,vac_tot_m,))
+### 4. get spatial data for join (if needed)
 
-### 2. get spatial data for join (if needed)
-
-# 2.1 AGOL version
+# 4.1 AGOL version
 # from services list of arcgis server: use query form to get all records, for example: Area_acres > 0, format geojson, then copy URL of resulting geojson object 
 # works but lacks muni_id and towns are spelled all caps
 
@@ -57,30 +66,30 @@ vac <- vac %>%
 #geo_raw <- st_read(request)
 #st_crs(geo_raw)
 
-# 2.2 read shp from K drive version
+# 4.2 read shp from K drive version
 geo_shp <- read_sf("K:/DataServices/Datasets/Boundaries/Municipal/ma_municipalities.shp") 
 st_crs(geo_shp)
 
-# 3.  join b25004 table df to shp
+# 5.  join b25004 table df to shp
 shp_join <- geo_shp %>%
   left_join(.,
-            vac %>% select(-c(seq_id,municipal)),
+            calc %>% select(-c(seq_id,municipal)),
             by = c('muni_id' = 'muni_id')) %>% 
   arrange(muni_id) 
 
 
-# 4. sample chloropleth map
+# 6. sample chloropleth map
 
-# 4.1  plain black outline
+# 6.1  plain black outline
 # ggplot(shp_join) +
 #   geom_sf(fill = "white", color = "black", linewidth = 0.3) +
 #   theme_void()
- 
-# 4.2 with chloropleth on vac_pct column
+
+# 6.2 with chloropleth on vac_pct column Vacancy Percentage
 
 map_title = paste0(input_year," Vacancy Status")
 
-test_plot <- ggplot(shp_join) +
+vac_plot <- ggplot(shp_join) +
   geom_sf(aes(fill = vac_pct), linewidth = 0, alpha = 0.9) +
   theme_void() +
   scale_fill_viridis_c(
@@ -130,17 +139,65 @@ test_plot <- ggplot(shp_join) +
     legend.position = c(0.7, 0.09)
   )
 
-test_plot
+vac_plot
 
-# 5 sample graph
-shp_join %>%
-  ggplot(aes(x = vac_pct)) +
-  geom_histogram(bins = 10, fill = "skyblue", color = "black") +
-  scale_x_log10()
+# 6.3 with chloropleth on avail_pct column Units For Rent or For Sale Percentage
+
+map_title = paste0(input_year," % Units Available for Sale or Rent")
+
+avail_plot <- ggplot(shp_join) +
+  geom_sf(aes(fill = avail_pct), linewidth = 0, alpha = 0.9) +
+  theme_void() +
+  scale_fill_viridis_c(
+    trans = "log", breaks = c(1, 5, 10, 20, 50, 100),
+    name = "pct",
+    guide = guide_legend(
+      keyheight = unit(3, units = "mm"),
+      keywidth = unit(12, units = "mm"),
+      label.position = "bottom",
+      title.position = "top",
+      nrow = 1
+    )
+  ) +
+  labs(
+    title = map_title,
+    subtitle = "percentage of total",
+    caption = "Data: ACS | B25004, B25024"
+  ) +
+  theme(
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    plot.title = element_text(
+      size = 20, hjust = 0.01, color = "#4e4d47",
+      margin = margin(
+        b = -0.1, t = 0.4, l = 2,
+        unit = "cm"
+      )
+    ),
+    plot.subtitle = element_text(
+      size = 15, hjust = 0.01,
+      color = "#4e4d47",
+      margin = margin(
+        b = -0.1, t = 0.43, l = 2,
+        unit = "cm"
+      )
+    ),
+    plot.caption = element_text(
+      size = 10,
+      color = "#4e4d47",
+      margin = margin(
+        b = 0.3, r = -99, t = 0.3,
+        unit = "cm"
+      )
+    ),
+    legend.position = c(0.7, 0.09)
+  )
+
+avail_plot
 
 
-## 6. EXPORT THE DATA to file for input year
+## 7. EXPORT THE DATA to file for input year
 write.csv(vac, paste0(exp_path, "SHoP_b25004_b25024_vacancy_",input_year,".csv"))
-
-
 
